b607f94af5251d9e69a73b08fdc6f0b39fdf8c35
Seva Zhidkov <seva@zhidkoff.com>
2022-09-10 11:38:30 +0300

    fix build error on @solana/spl-token

--- src/components/NetworkSwitcher.tsx
+++ src/components/NetworkSwitcher.tsx
@@ -5,8 +5,6 @@ import { useNetworkConfiguration } from '../contexts/NetworkConfigurationProvide
 const NetworkSwitcher: FC = () => {
   const { networkConfiguration, setNetworkConfiguration } = useNetworkConfiguration();
 
-  console.log(networkConfiguration);
-
   return (
     <label className="cursor-pointer label">
       <a>Network</a>
